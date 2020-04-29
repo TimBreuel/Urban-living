9e74b8e26507481da5b29d72a6a7b448c47baec5
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2020-04-29 11:08:56 +0200

    Update/ about us / responsive

--- js/app.js
+++ js/app.js
@@ -1225,57 +1225,112 @@ const passwordAgainCheck = (selectorID, pw, txt) => {
 
 ///////////////////
 //ABOUT US CREATER
-
 const aboutUsCreater = () => {
     headline.innerText = 'About Us'
-    productContainer.innerHTML = `
-    <div class="box1">
-            <div class="boxImage col4"><img src="./img/WorkingTogether.png" alt=""></div>
-            <div class="boxContent col8">
-                <h3>Our Team</h3>
 
-                <p>Looking for a new a interesting job? We encourage interdiciplinary working and fundamentally
-                    believe in the power of teamwork. Whether you're a Marketing Profession, Programmer or a
-                    Logistics Lover - don't hestiate to get in contact with us about open opportunities.</p>
-                <br>
-                <span>JOBS AT URBAN LIVING: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
-                <span>Send us yor Initiave application today. <a href="#">Go to chat</a></span>
-
+    let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth > 600) {
+        productContainer.innerHTML = `
+        <div class="box1">
+                <div class="boxImage col4"><img src="./img/WorkingTogether.png" alt=""></div>
+                <div class="boxContent col8">
+                    <h3>Our Team</h3>
+    
+                    <p>Looking for a new a interesting job? We encourage interdiciplinary working and fundamentally
+                        believe in the power of teamwork. Whether you're a Marketing Profession, Programmer or a
+                        Logistics Lover - don't hestiate to get in contact with us about open opportunities.</p>
+                    <br>
+                    <span>JOBS AT URBAN LIVING: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
+                    <span>Send us yor Initiave application today. <a href="#">Go to chat</a></span>
+    
+                </div>
             </div>
-        </div>
-
-        <div class="box2">
-
-            <div class="boxContent2 col8">
-                <h3>Contact Us</h3>
-
-                <p>We strive to give the best possible customer service. Should you have any questions or need to
-                    talk to someone about an order, you can get in touch with us the following ways:</p>
-                <br>
-                <span>Phone: 0402 558 33</span>
-                <span>Email: maxmusterman@gmail.com</span>
-                <span>LinkedIn: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
-                <span>Chat: <a href="#">Go to chat</a></span>
-
+    
+            <div class="box2">
+    
+                <div class="boxContent2 col8">
+                    <h3>Contact Us</h3>
+    
+                    <p>We strive to give the best possible customer service. Should you have any questions or need to
+                        talk to someone about an order, you can get in touch with us the following ways:</p>
+                    <br>
+                    <span>Phone: 0402 558 33</span>
+                    <span>Email: maxmusterman@gmail.com</span>
+                    <span>LinkedIn: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
+                    <span>Chat: <a href="#">Go to chat</a></span>
+    
+                </div>
+                <div class="boxImage2 col4"><img src="./img/CustomerService.png" alt=""></div>
             </div>
+    
+            <div class="box3">
+                <div class="boxImage col4"><img src="./img/OfficeHours.png" alt=""></div>
+                <div class="boxContent col8">
+                    <h3>Our Office</h3>
+    
+                    <p>Located in the heart of Hamburg, our dedicated teams with their passion for all things interior,
+                        work to provide you with the best possible online shopping experience.
+                        You can find us here: </p>
+                    <br>
+                    <span>Urban Living GmbH</span>
+                    <span>Landwehr 29</span>
+                    <span>22087 Hamburg</span>
+    
+                </div>
+    
+            </div>
+        `
+    } else {
+        productContainer.innerHTML = `
+        <div class="box1">
+                <div class="boxImage col4"><img src="./img/WorkingTogether.png" alt=""></div>
+                <div class="boxContent col8">
+                    <h3>Our Team</h3>
+    
+                    <p>Looking for a new a interesting job? We encourage interdiciplinary working and fundamentally
+                        believe in the power of teamwork. Whether you're a Marketing Profession, Programmer or a
+                        Logistics Lover - don't hestiate to get in contact with us about open opportunities.</p>
+                    <br>
+                    <span>JOBS AT URBAN LIVING: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
+                    <span>Send us yor Initiave application today. <a href="#">Go to chat</a></span>
+    
+                </div>
+            </div>
+    
+            <div class="box2">
             <div class="boxImage2 col4"><img src="./img/CustomerService.png" alt=""></div>
-        </div>
-
-        <div class="box3">
-            <div class="boxImage col4"><img src="./img/OfficeHours.png" alt=""></div>
-            <div class="boxContent col8">
-                <h3>Our Office</h3>
-
-                <p>Located in the heart of Hamburg, our dedicated teams with their passion for all things interior,
-                    work to provide you with the best possible online shopping experience.
-                    You can find us here: </p>
-                <br>
-                <span>Urban Living GmbH</span>
-                <span>Landwehr 29</span>
-                <span>22087 Hamburg</span>
-
+                <div class="boxContent2 col8">
+                    <h3>Contact Us</h3>
+    
+                    <p>We strive to give the best possible customer service. Should you have any questions or need to
+                        talk to someone about an order, you can get in touch with us the following ways:</p>
+                    <br>
+                    <span>Phone: 0402 558 33</span>
+                    <span>Email: maxmusterman@gmail.com</span>
+                    <span>LinkedIn: <a href="https://www.linkedin.com/jobs/tracker/saved/">Go to site</a></span>
+                    <span>Chat: <a href="#">Go to chat</a></span>
+    
+                </div>
+                
             </div>
+    
+            <div class="box3">
+                <div class="boxImage col4"><img src="./img/OfficeHours.png" alt=""></div>
+                <div class="boxContent col8">
+                    <h3>Our Office</h3>
+    
+                    <p>Located in the heart of Hamburg, our dedicated teams with their passion for all things interior,
+                        work to provide you with the best possible online shopping experience.
+                        You can find us here: </p>
+                    <br>
+                    <span>Urban Living GmbH</span>
+                    <span>Landwehr 29</span>
+                    <span>22087 Hamburg</span>
+    
+                </div>
+    
+            </div>
+        `
+    }
 
-        </div>
-    `
 }
